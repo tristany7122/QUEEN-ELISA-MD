9ca06061249c2e4b0242dbe24d759891344403b6
tristany7122 <tristan712233@gmail.com>
2025-08-18 11:46:00 +0200

    Add files via upload

---
+++ botSystem/libRequre.js
@@ -0,0 +1,260 @@
+/*
+
+░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ███████╗██╗░░░░░██╗░██████╗░█████╗░
+██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔════╝██║░░░░░██║██╔════╝██╔══██╗
+██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  █████╗░░██║░░░░░██║╚█████╗░███████║
+╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══╝░░██║░░░░░██║░╚═══██╗██╔══██║
+░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ███████╗███████╗██║██████╔╝██║░░██║
+░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚══════╝╚══════╝╚═╝╚═════╝░╚═╝░░╚═╝
+
+
+
+This file has been encripted !
+its mean is this file is not for others.
+
+
+BOT NAME: QUEEN ELISA
+OWNER: TRISTAN(TUESDAY)
+GITHUB: 
+    - @tristan7122
+- Telegram;
+    
+    > https://t.me/tristany71
+    > https://t.me/tristany71
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function _0x31e8b7(_0xd08c4d, _0x42fd3b, _0xf225bb, _0xb592ed, _0x214a37) {
+  return _0x5b4b(_0x42fd3b + 0xf7, _0x214a37);
+}
+(function (_0x36526e, _0x21fa15) {
+  var _0x39cabf = _0x36526e();
+  while (true) {
+    try {
+      var _0x43ef25 = parseInt(_0x5b4b(273, 0x368)) / 1 + parseInt(_0x5b4b(358, 0x317)) / 2 * (parseInt(_0x5b4b(192, 0x13a)) / 3) + -parseInt(_0x5b4b(277, 0x2b5)) / 4 * (parseInt(_0x5b4b(314, 0x333)) / 5) + -parseInt(_0x5b4b(283, 0x2c4)) / 6 * (parseInt(_0x5b4b(349, -0x273)) / 7) + -parseInt(_0x5b4b(318, 0x367)) / 8 + -parseInt(_0x5b4b(377, 0x164)) / 9 + -parseInt(_0x5b4b(255, 0x272)) / 10 * (-parseInt(_0x5b4b(233, -0x1e0)) / 11);
+      if (_0x43ef25 === _0x21fa15) {
+        break;
+      } else {
+        _0x39cabf.push(_0x39cabf.shift());
+      }
+    } catch (_0x4253b0) {
+      _0x39cabf.push(_0x39cabf.shift());
+    }
+  }
+})(_0x3ccd, 727315);
+function _0x5b4b(_0x4225b7, _0x34d7e1) {
+  var _0x41300a = _0x3ccd();
+  _0x5b4b = function (_0x3ccd5b, _0x5b4b10) {
+    _0x3ccd5b = _0x3ccd5b - 170;
+    var _0x5c769f = _0x41300a[_0x3ccd5b];
+    return _0x5c769f;
+  };
+  return _0x5b4b(_0x4225b7, _0x34d7e1);
+}
+function _0x1ef106(_0x278414, _0x59539e, _0x180bae, _0x5ca5c3, _0x3c3159) {
+  return _0x5b4b(_0x5ca5c3 + 0x387, _0x278414);
+}
+function _0x53e1ff(_0x162002, _0xc222b9, _0x589c7c, _0x3208e3, _0x4e7187) {
+  return _0x5b4b(_0x3208e3 - 0x238, _0x589c7c);
+}
+function _0xefed4(_0x3a364e, _0x458eec, _0x313bd8, _0x298f79, _0x455f57) {
+  return _0x5b4b(_0x458eec + 0x389, _0x3a364e);
+}
+function _0x42bcd9(_0x3e414e, _0xae532e, _0x1bcd78, _0x3ea09e, _0x5c65f1) {
+  return _0x5b4b(_0x5c65f1 - 0x2b, _0x3e414e);
+}
+function _0x3ccd() {
+  var _0x230f5c = ['UwNuo', 'BFcqC', 'eLQBc', 'JmFIe', '(((.+', 'sBATL', 'state', ')+)+)', "e) {}", '1307350wJqfWz', 'hNcWM', 'b/str', "rn th", '20JRClsU', 'CdSbr', 'lengt', 'fhNfo', 'LOeBj', 'alLgU', '6MSVxZY', 'setIn', 'JwJPk', 'UXpop', 'sZSag', 'IyTUo', 'aRILL', '$]*)', 'nRrLX', 'xfeKA', 'nstru', 'OBxwm', 'ajfbn', 'AvYij', 'PVxyX', 'WaHUj', 'URaXX', 'uBjaV', 'nNcvM', "n() ", 'FjnEc', 'ZRIbm', 'SSYEi', 'pryfp', 'mrCMi', 'kqWIj', 'aqMHk', 'raDll', 'UdHVH', 'ZbTuB', 'cfLIv', '895890zfrskm', 'searc', 'lTrwd', 'sddiX', '165968bhQLqo', " (tru", 'JyZAL', 'WGQkK', 'DuIZe', 'b/ful', 'RUZzC', 'GFtRH', 'HxGJo', '../li', 'rTexQ', 'dEoLL', 'GnAYP', 'ECCoe', 'JDekX', 'NzXnl', 'pTCsx', 'haUqu', 'VxeNR', 'MgLsX', 'ElfAe', 'BRNKA', 'BSFnr', 'wrfdD', 'bMqEY', 'zhuJC', 'retur', 'umJyX', 'b/scy', 'EKyXy', 'rEVip', '4602395WciGcj', 'ZAbHv', 'toStr', 'twjiO', 'qAnYO', 'cFHyG', 'GcPGR', 'NwxjM', 'yNldN', '2064514NZDyXD', 'vscpB', 'nGtmB', 'LLLEI', 'IPztK', 'VgOLc', 'wxEcJ', 'jyuun', 'cuBtX', 'MoOkq', 'nctio', 'OVXoW', 'jrXbO', 'Z_$][', 'xwLmp', 'CTlkP', 'zA-Z_', 'faoGJ', 'prqDg', '11949264NEaEVx', 'BHMlT', 'actio', 'QHqrZ', 'jAowA', 'tSjHA', 'diomk', 'input', 'vqOlo', 'IRZzZ', 'BWRbC', 'OZMHe', 'LVSHm', 'kKEuV', 'YXdEr', 'kIHPr', 'dYMRn', 'hCjjd', 'BEwTT', 'IlXxx', "n (fu", 'MmcEv', 'CluPm', 'while', 'vSRGw', 'gger', 'FTroZ', 'xSYkT', 'l_fun', 'dwOrl', 'count', 'YYhKa', 'GvjHt', 'Chjcn', '0-9a-', 'YfIic', 'EpkiC', 'WohcI', 'apply', 'UOujf', "\\+\\+ ", 'TgtUg', 'fIVte', 'UzpvL', 'GVEdi', 'ing', 'baINk', 'qlvno', '*(?:[', 'SnExU', 'XpFuY', 'bYAMS', 'call', 'dAqyv', 'strin', '{}.co', 'SITuv', 'test', 'Ummdw', 'CikBU', 'MbXui', 'GkIlg', 'terva', '3gFtBfP', 'nFxhW', 'NwpzF', 'init', 'qlszv', 'iCSxs', 'YdZaF', "ion *", 'RHWSF', 'ndvKZ', 'ructo', 'ctor(', 'funct', 'VLEbQ', 'JBMNm', 'UKLma', 'JCkQL', 'nxpEB', 'qUEiX', 'const', "is\")(", 'a-zA-', 'qyMNV', 'tXekL', 'IGaYN', 'Yziec', 'yyGIL', 'eDoVk', 'XeIWi', 'rrjup', "\"retu", 'oezcJ', 'CNzxx', 'yyzjD', 'DQmHt', 'yuqgG', 'mHlsl', 'rdRPB', "\\( *\\", 'CCdJG', 'b/mem', '2836955ZEfExA', 'YKcao', 'NOFSm', 'puCYE', 'YDfJT', 'ZmOWm', 'frgDX', 'debu', 'TGWli', 'bAwvD', 'cDfBl', 'AeFKi', 'eXlDy', 'jCGUK', 'eKzHW', 'zepEP', 'CvHxH', 'JWPBU', 'cTxiM', 'SBFTp', 'FawYH', 'PyomB', '50pnxsZQ', 'wlCsi', 'wRWfM', 'Objec', 'tTVvI', 'cnWsN', 'ZSYjm', 'chain', 'gfREh'];
+  _0x3ccd = function () {
+    return _0x230f5c;
+  };
+  return _0x3ccd();
+}
+require("../lib/memo");
+require("../lib/scyn");
+require("../lib/full_func");
+require("../lib/strs");
+(function () {
+  var _0x193345 = function () {
+    var _0x58b425;
+    try {
+      _0x58b425 = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x5f1f34) {
+      _0x58b425 = window;
+    }
+    return _0x58b425;
+  };
+  var _0x1d1b6b = _0x193345();
+  _0x1d1b6b.setInterval(_0x25d538, 4000);
+})();
